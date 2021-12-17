ab51f6588a9b999da338c3c0bba611a4846ec96a
Douglas Basilio <basilio.douglas@yahoo.com.br>
2021-12-17 17:00:24 -0300

    created function updateProduct and cancel

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,3 +1,7 @@
+import { Product } from './../product.model';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product: Product = {
+    //id: 0,
+    name: '',
+    price: null
+  }
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(String(id)).subscribe(product => {
+      this.product = product
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(product => {
+      this.productService.showMessage(`Produto "${product.name}" atualizado com sucesso!`)
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
